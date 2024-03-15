cfe4fc5e92a6259ce884af5fc8c6306db39f8657
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2024-03-15 17:03:22 -0600

    hf_model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from langchain_community.llms import Ollama
 from langchain_openai import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import gradio as gr
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
@@ -15,6 +16,7 @@ from langchain_core.messages import HumanMessage, AIMessage
 from langchain.chains import create_history_aware_retriever
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
+from langchain_community.llms import HuggingFaceEndpoint
 
 output_parser = StrOutputParser()
 
@@ -38,8 +40,7 @@ class LLM(ABC):
 class OpenAIModel(LLM):
     def __init__(self, name: str, api_key: str):
         super().__init__(name)
-        self.api_key = api_key
-        self.llm = ChatOpenAI(model=self.name, openai_api_key=openai_api_key)
+        self.llm = ChatOpenAI(model=self.name, openai_api_key=api_key)
         self.embeddings = OpenAIEmbeddings()
 
 
@@ -50,35 +51,48 @@ class OllamaModel(LLM):
         self.embeddings = OllamaEmbeddings()
 
 
-class VectorStore:
+class HFModel(LLM):
+    def __init__(self, name, endpoint_url, token):
+        super().__init__(name)
+        self.llm = HuggingFaceEndpoint(
+            endpoint_url=endpoint_url,
+            max_new_tokens=1,
+            top_k=50,
+            temperature=0.1,
+            repetition_penalty=1.03,
+            huggingfacehub_api_token=token
+        )
+        self.embeddings = HuggingFaceInferenceAPIEmbeddings(
+            api_key=token, model_name="sentence-transformers/all-MiniLM-l6-v2"
+        )
+
+
+class Docs:
     def __init__(self):
-        self.docs = None
-        self.vector = None
-        self.embeddings = None
+        self.docs = []
 
-    def set_embeddings(self, embeddings):
-        self.embeddings = embeddings
-
-    def set_docs(self, filepath):
+    def set_pdf_docs(self, filepath):
         loader = PyPDFLoader(filepath)
         docs = loader.load()
         self.docs = docs
-        return docs
-
-    def set_vector(self):
-        text_splitter = RecursiveCharacterTextSplitter()
-        documents = text_splitter.split_documents(self.docs)
-        vector = FAISS.from_documents(documents, self.embeddings)
-        self.vector = vector
-
-    def get_vector(self):
-        return self.vector
 
     def get_docs(self):
         return self.docs
 
-    def get_embeddings(self):
-        return self.embeddings
+
+class VectorStore:
+    def __init__(self, docs: Docs):
+        self.docs = docs
+        self.vector = None
+
+    def set_vector(self, embeddings):
+        text_splitter = RecursiveCharacterTextSplitter()
+        documents = text_splitter.split_documents(self.docs.get_docs())
+        vector = FAISS.from_documents(documents, embeddings)
+        self.vector = vector
+
+    def get_vector(self):
+        return self.vector
 
 
 class Chain:
@@ -148,8 +162,8 @@ class ChatBot:
 
     def get_conversational_answer(self, message):
         answer = self.chain.get_chain().invoke(
-                {"chat_history": self.chat_history, "input": message}
-            )
+            {"chat_history": self.chat_history, "input": message}
+        )
         self.add_history(message, answer)
         return answer
 
@@ -173,20 +187,26 @@ if __name__ == "__main__":
     if model_name.lower() == "openai":
         openai_api_key = input("Ingrese la API key de OpenAI: ")
         chatbot_model = OpenAIModel("gpt-3.5-turbo", openai_api_key)
+    elif model_name.lower() == "hf":
+        hf_endpoint_url = input("input your HF endpoint url: ")
+        hf_token = input("input your HF token: ")
+        chatbot_model = HFModel("hf-model", hf_endpoint_url, hf_token)
     elif model_name.lower() == "ollama":
         chatbot_model = OllamaModel("llama2")
     else:
         print("Nombre de modelo no válido.")
         exit()
 
-    vectorstore = VectorStore()
-    vectorstore.set_docs(
+    user_docs = Docs()
+    user_docs.set_pdf_docs(
         "/home/jorge/Development/chat_with_PDF/La temporada invernal será más húmeda de lo normal y con precipitaciones.pdf")
-    vectorstore.set_embeddings(chatbot_model.embeddings)
-    vectorstore.set_vector()
+
+    vectorstore = VectorStore(user_docs)
+    vectorstore.set_vector(chatbot_model.get_embeddings())
 
     chatbot_chain = Chain(chatbot_model)
     chatbot_chain.set_retrieval_chain(vectorstore)
+
     chatbot = ChatBot(chatbot_chain)
 
     while True:
